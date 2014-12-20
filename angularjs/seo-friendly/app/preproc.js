c5dfa28ec77aa60424c1ee8fec4ca71c5f696375
analog-nico <nicolai.kamenzky@testrails.org>
2014-12-20 17:08:27 -0500

    Serving the initial template

--- angularjs/seo-friendly/app/preproc.js
+++ angularjs/seo-friendly/app/preproc.js
@@ -1,16 +1,33 @@
 'use strict';
 
+var fs = require('fs');
+var path = require('path');
 var parseurl = require('parseurl');
 var store = require('../../../helpers/store.js');
 
 
+var templates = {
+    'list.html': fs.readFileSync(path.join(__dirname, './partials/list.html')),
+    'detail.html': fs.readFileSync(path.join(__dirname, './partials/detail.html'))
+};
+
+function getTemplate(name) {
+    return '<script type="text/ng-template" id="partials/' + name + '">\n' +
+        templates[name] + '\n' +
+        '</script>';
+}
+
+
 module.exports = function (req, res, next) {
 
+    req.initialTemplate = '';
     req.initialData = null;
 
     var url = parseurl(req);
 
     if (url.pathname === '/') {
+
+        req.initialTemplate = getTemplate('list.html');
 
         store.list(function (err, ret) {
             if (!err) {
@@ -24,12 +41,18 @@ module.exports = function (req, res, next) {
 
     } else if (url.pathname.match(/^\/edit\//)) {
 
+        req.initialTemplate = getTemplate('detail.html');
+
         store.read(url.pathname.substr('/edit/'.length), function (err, ret) {
             if (!err) {
                 req.initialData = ret;
             }
             next();
         });
+
+    } else if (url.pathname === '/new') {
+
+        req.initialTemplate = getTemplate('detail.html');
 
     } else {
         next();
