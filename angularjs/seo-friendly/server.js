b83b0d69414d0a1d33d1d57205d689b293943bc8
analog-nico <nicolai.kamenzky@testrails.org>
2014-12-20 14:55:22 -0500

    Groundwork for the seo-friendly angularjs implementation

---
+++ angularjs/seo-friendly/server.js
@@ -0,0 +1,35 @@
+'use strict';
+
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
+var api = require('../../helpers/api.js');
+var serveSpa = require('serve-spa');
+
+
+var app = express();
+
+// Serving the bower components still with express.static
+
+app.use(express.static(path.join(__dirname, '../../bower_components')));
+
+
+// Providing the REST API
+
+app.use(bodyParser.json());
+app.get('/api/projects', api.list);
+app.post('/api/projects', api.create);
+app.get('/api/projects/:id', api.read);
+app.put('/api/projects/:id', api.update);
+app.delete('/api/projects/:id', api.purge);
+
+
+// Finally serving the app with serve-spa
+// (Due to the pushState support serve-spa must be registered last.)
+
+serveSpa(app, path.join(__dirname, './app'));
+
+
+app.listen(3000, function () {
+    console.log('Express server listening on port 3000');
+});
